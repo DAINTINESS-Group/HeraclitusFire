1973eb6a907af2a57c3fc9a8e94ba5f5189aa15c
pvassil <panos.vassiliadis@gmail.com>
2019-12-13 20:09:52 +0200

    inv. gamma

---
+++ src/patternassessment/tablepatterns/InverseGammaAssessment.java
@@ -0,0 +1,45 @@
+package patternassessment.tablepatterns;
+
+import java.util.ArrayList;
+
+import datamodel.TableDetailedStatsElement;
+
+public class InverseGammaAssessment extends PatternAssessmentTemplateMethod {
+
+	public InverseGammaAssessment(ArrayList<TableDetailedStatsElement> pInputTupleCollection, String projectName,
+			String pOutputFolderWithPatterns, String globalAppendLogPath, double alpha) {
+		super(pInputTupleCollection, projectName, pOutputFolderWithPatterns, globalAppendLogPath, alpha);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getTestName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PatternAssessmentResult constructResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[][] computeContingencyTable(PatternAssessmentResult par) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean decideIfPatternHolds(PatternAssessmentResult par) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String constructResultDescription(PatternAssessmentResult par) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
