df4f6152b6ab81b0e139bfb0bf6bca7d5f475435
alexvou <voulale@gmail.com>
2020-02-12 14:22:19 +0200

    Corrected assertion deltas for testing FisherExactTestWrapper

--- src/test/patternassessment/fisher/exact/test/FisherExactTestWrapperTest.java
+++ src/test/patternassessment/fisher/exact/test/FisherExactTestWrapperTest.java
@@ -12,42 +12,42 @@ public class FisherExactTestWrapperTest {
 	final void testGetFisherSingleTailPValue() {
 		int [][] counts = {{13,2},{61,12}};
 		FisherExactTestWrapper fet = new FisherExactTestWrapper(counts);
-		assertEquals(0.5588, fet.getFisherSingleTailPValue(), 3);        
+		assertEquals(0.5588, fet.getFisherSingleTailPValue(), 0.001);        
 	}
 
 	@Test
 	final void testGetFisherOppositeTailPValue() {
 		int [][] counts = {{13,2},{61,12}};
 		FisherExactTestWrapper fet = new FisherExactTestWrapper(counts);
-		assertEquals(0.4412, fet.getFisherOppositeTailPValue(), 3);        
+		assertEquals(0.4412, fet.getFisherOppositeTailPValue(), 0.001);        
 	}
 
 	@Test
 	final void testGetFisherTwoTailedPValueAtlas() {
 		int [][] counts = {{13,2},{61,12}};
 		FisherExactTestWrapper fet = new FisherExactTestWrapper(counts);
-		assertEquals(1.0, fet.getFisherTwoTailedPValue(), 3);        
+		assertEquals(1.0, fet.getFisherTwoTailedPValue(), 0.001);        
 	}
 	
 	@Test
 	final void testGetFisherSingleTailPValueArbitrary() {
 		int [][] counts = {{7,2},{5,6}};
 		FisherExactTestWrapper fet = new FisherExactTestWrapper(counts);
-		assertEquals(0.1569, fet.getFisherSingleTailPValue(), 3);
+		assertEquals(0.1569, fet.getFisherSingleTailPValue(), 0.001);
 	}
 	
 	@Test
 	final void testGetFisherOppositeTailPValueArbitrary() {
 		int [][] counts = {{7,2},{5,6}};
 		FisherExactTestWrapper fet = new FisherExactTestWrapper(counts);
-		assertEquals(0.0399, fet.getFisherOppositeTailPValue(), 3);
+		assertEquals(0.0399, fet.getFisherOppositeTailPValue(), 0.001);
 	}
 
 	@Test
 	final void testGetFisherTwoTailedPValueArbitrary() {
 		int [][] counts = {{7,2},{5,6}};
 		FisherExactTestWrapper fet = new FisherExactTestWrapper(counts);
-		assertEquals(0.1968, fet.getFisherTwoTailedPValue(), 3);
+		assertEquals(0.1968, fet.getFisherTwoTailedPValue(), 0.001);
 	}
 
 }//end class
