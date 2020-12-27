df3972a37f26217dbc587020d431bfd53252d5d8
george50450 <gtkalampokis@gmail.com>
2020-12-27 16:27:30 +0200

    Com. 6: Added tests for SchemaLevelInfo(Atlas, Egee)

--- src/test/mainEngine/AtlasSchemaHeartbeatTest.java
+++ src/test/mainEngine/AtlasSchemaHeartbeatTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import datamodel.SchemaHeartbeatElement;
 import mainEngine.SchemaStatsMainEngine;
 
-class AtlasSchemaHeartBeatTest {
+class AtlasSchemaHeartbeatTest {
 	private static SchemaStatsMainEngine schemaStatsMainEngine; 
 	private static ArrayList<String> headerExpected; 
 	
--- src/test/mainEngine/AtlasSchemaLevelTest.java
+++ src/test/mainEngine/AtlasSchemaLevelTest.java
@@ -4,16 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import datamodel.MonthSchemaStats;
 import datamodel.SchemaHeartbeatElement;
+import datamodel.SchemaLevelInfo;
 import mainEngine.SchemaStatsMainEngine;
 
-class AtlasMonthlyStatsTest {
+class AtlasSchemaLevelTest {
 	private static SchemaStatsMainEngine schemaStatsMainEngine; 
 	
 	@BeforeAll
@@ -22,36 +21,29 @@ class AtlasMonthlyStatsTest {
 	}
 	
 	@Test
-	void testAtlasMonthlySchemaStats() {
+	void testAtlasSchemaLevelInfo() {
 		ArrayList<String> header = new ArrayList<String>();
 		ArrayList<SchemaHeartbeatElement> inputTupleCollection = new ArrayList<SchemaHeartbeatElement>();
 		int numRows = schemaStatsMainEngine.loadData("resources/Atlas/results/SchemaHeartbeat.tsv", "\t", true, 28, header, inputTupleCollection);
 		assertEquals(numRows - 1, 85); //check without header (commits)
-	
-		File statsFileProduced = new File("resources/test/Profiling/Atlas_MonthlySchemaStats.tsv"); 
+		
+		File statsFileProduced = new File("resources/test/Profiling/Atlas_SchemaLevelInfo.tsv"); 
 		Long originalTimeStamp = statsFileProduced.lastModified();
-		ArrayList<MonthSchemaStats> monthlySchemaStatsCollection = schemaStatsMainEngine.extractMonthlySchemaStats("Atlas", inputTupleCollection, "resources/test/Profiling", true);
-		assertEquals(monthlySchemaStatsCollection.size(), 33);
+		SchemaLevelInfo schemaLevelInfo = schemaStatsMainEngine.extractSchemaLevelInfo("Atlas", inputTupleCollection, "resources/test/Profiling", true);
 		Long newTimeStamp = statsFileProduced.lastModified();
 		assertTrue(newTimeStamp > originalTimeStamp);
 		
+		//schemaLevelInfo.get
 		//Truth sum-values were manually calculated by SchemaHeartBeat.tsv file. 
 		int truth_active = 70;
 		int truth_turf = 51;
 		int truth_reed = 19;
 		
-		int sum_active = 0;
-		int sum_turf = 0;
-		int sum_reed = 0;
-		for(MonthSchemaStats mnt: monthlySchemaStatsCollection) {
-			sum_active += mnt.getActiveCommits();
-			sum_turf += mnt.getTurfs();
-			sum_reed += mnt.getReeds();
-		}
+		assertEquals(schemaLevelInfo.getActiveCommits(), truth_active);
+		assertEquals(schemaLevelInfo.getTurfs(), truth_turf);
+		assertEquals(schemaLevelInfo.getReeds(), truth_reed);
 		
-		assertEquals(sum_active,truth_active);
-		assertEquals(sum_turf,truth_turf);
-		assertEquals(sum_reed,truth_reed);
+		assertEquals(schemaLevelInfo.getProjectDurationInMonths(), 32);
 	}
 
 }
